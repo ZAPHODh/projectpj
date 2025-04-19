c07e6b985817c9fb773111e5b6a0ead1e260d56a
zaphodh <luispaulo.ni@gmail.com>
2025-04-19 09:59:48 -0300

    fixed type error in avatar group

--- src/components/ui/avatar-group.tsx
+++ src/components/ui/avatar-group.tsx
@@ -4,27 +4,27 @@ import { cloneElement, Children, forwardRef, useMemo } from "react";
 
 import type { ElementRef, HTMLAttributes, ReactElement } from "react";
 
-// ================================== //
-
 type TAvatarGroupRef = ElementRef<"div">;
 type TAvatarGroupProps = HTMLAttributes<HTMLDivElement> & { max?: number; spacing?: number };
 
 const AvatarGroup = forwardRef<TAvatarGroupRef, TAvatarGroupProps>(({ className, children, max = 1, spacing = 10, ...props }, ref) => {
     const avatarItems = Children.toArray(children) as ReactElement[];
 
+
     const renderContent = useMemo(() => {
+        const typedAvatarItems = avatarItems as ReactElement<{ className?: string; style?: React.CSSProperties }>[];
         return (
             <>
-                {avatarItems.slice(0, max).map((child, index) => {
+                {typedAvatarItems.slice(0, max).map((child, index) => {
                     return cloneElement(child, {
                         className: cn(child.props.className, "border-2 border-bg-primary"),
                         style: { marginLeft: index === 0 ? 0 : -spacing, ...child.props.style },
                     });
                 })}
 
-                {avatarItems.length > max && (
+                {typedAvatarItems.length > max && (
                     <div
-                        className={cn("relative flex items-center justify-center rounded-full border-2 border-bg-primary bg-bg-tertiary", avatarItems[0].props.className)}
+                        className={cn("relative flex items-center justify-center rounded-full border-2 border-bg-primary bg-bg-tertiary", typedAvatarItems[0].props.className)}
                         style={{ marginLeft: -spacing }}
                     >
                         <p>+{avatarItems.length - max}</p>
@@ -43,6 +43,5 @@ const AvatarGroup = forwardRef<TAvatarGroupRef, TAvatarGroupProps>(({ className,
 
 AvatarGroup.displayName = "AvatarGroup";
 
-// ================================== //
 
 export { AvatarGroup };
