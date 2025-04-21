4a3b09f19ef26131db377529b8435b1737004cd3
zaphodh <luispaulo.ni@gmail.com>
2025-04-20 22:13:37 -0300

    button internalizado

--- src/components/widgets/import-customer.tsx
+++ src/components/widgets/import-customer.tsx
@@ -5,13 +5,16 @@ import { Button } from "../ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "../ui/dropdown-menu";
 import { CustomerImportInfo } from "./customer-import-info";
 import { DocumentUploader } from "./document-uploader";
+import { useTranslations } from "next-intl";
 
 export function ImportCustomer() {
     const [open, setOpen] = useState(false)
+    const t = useTranslations('customer.import.alert');
+
     return (
         <DropdownMenu open={open} onOpenChange={setOpen}>
             <DropdownMenuTrigger asChild>
-                <Button variant="outline">Import Customers</Button>
+                <Button variant="outline">{t('button')}</Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-96 p-4" align="end">
                 <CustomerImportInfo />
